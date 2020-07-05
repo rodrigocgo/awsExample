ecd6c81936a48a7c174e16de9436b21facdf980c
rodrigo.gon95 <rodrigo.gon95@gmail.com>
2020-07-04 22:28:11 -0300

    alteracao

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,4 +3,6 @@
 sudo apt-get update
 sudo apt-get install -y python2.7 python-pip
 pip install Flask
-pip install -r requirements.txt
+pip install flask-wtf
+pip install boto3
+
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
 cd /home/ubuntu
-export FLASK_APP=hello.py
+
+git clone https://github.com/rodrigocgo/awsExample.git
+
+cd awsExample
+
+sudo cp etc/rc.local /etc/
+
+VIRTUALENV=/home/ubuntu/venv
+
+pushd $(dirname "$0")
+
+source $VIRTUALENV/bin/activat
+
+export FLASK_APP=/home/ubuntu/awsExample/app.py
+
 flask run --host=0.0.0.0 --port 8080 >/dev/null 2>&1 &
+
+
